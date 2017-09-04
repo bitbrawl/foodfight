63bc2c6d8f2732756352d341641de26a0cdb6328
Finn Voichick <fvoichick@gmail.com>
2017-09-04 06:37:19 -0500

    Time-based Clock

--- src/main/java/org/bitbrawl/foodfight/player/Clock.java
+++ src/main/java/org/bitbrawl/foodfight/player/Clock.java
@@ -1,39 +1,84 @@
 package org.bitbrawl.foodfight.player;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 import org.bitbrawl.foodfight.field.Field;
 
 public final class Clock {
 
-	private long timeSpentBeforeThisTurn = 0, timeSpentThisTurn = 0;
-	private int turnNumber = 0;
+	private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
+	private volatile long timeSpentBeforeThisTurn = 0;
+	private volatile int turnNumber = 0;
+	private volatile long startTime;
+	private volatile boolean isRunning = false;
 
 	Clock() {
 	}
 
 	void startTurn(int turnNumber) {
+
+		lock.writeLock().lock();
 		this.turnNumber = turnNumber;
-		timeSpentBeforeThisTurn += timeSpentThisTurn;
-		timeSpentThisTurn = 0;
+		startTime = System.nanoTime();
+		isRunning = true;
+		lock.writeLock().unlock();
+
 	}
 
-	public void increment() {
-		timeSpentThisTurn++;
+	public long getTimeSpent(TimeUnit unit) {
+		return unit.convert(getTimeSpent(), TimeUnit.NANOSECONDS);
 	}
 
-	public long getTotalTimePointsSpent() {
-		return timeSpentBeforeThisTurn + timeSpentThisTurn;
+	private long getTimeSpent() {
+		lock.readLock().lock();
+		long result = timeSpentBeforeThisTurn;
+		if (isRunning)
+			result += getTimeSpentThisTurn();
+		lock.readLock().unlock();
+		return result;
 	}
 
-	public double getTimeLeft() {
-		return (double) getTotalTimePointsSpent() / TOTAL_TIME_POINTS;
+	public long getTimeSpentThisTurn(TimeUnit unit) {
+		return unit.convert(getTimeSpentThisTurn(), TimeUnit.NANOSECONDS);
 	}
 
-	public double getTimeLeftThisTurn() {
-		long timeLeft = TOTAL_TIME_POINTS - timeSpentBeforeThisTurn;
+	private long getTimeSpentThisTurn() {
+		lock.readLock().lock();
+		long result = System.nanoTime() - startTime;
+		lock.readLock().unlock();
+		return result;
+	}
+
+	public long getTimeLeft(TimeUnit unit) {
+		return unit.convert(TIME_LIMIT - getTimeSpent(), TimeUnit.NANOSECONDS);
+	}
+
+	public long getTimeLeftThisTurn(TimeUnit unit) {
+		lock.readLock().lock();
+		long timeLeft = TIME_LIMIT - timeSpentBeforeThisTurn;
 		long timePerTurn = timeLeft / (Field.TOTAL_TURNS - turnNumber);
-		return (double) (timePerTurn - timeSpentThisTurn) / timePerTurn;
+		long timeLeftThisTurn = timePerTurn - getTimeSpentThisTurn();
+		lock.readLock().unlock();
+		return unit.convert(timeLeftThisTurn, TimeUnit.NANOSECONDS);
 	}
 
-	public static final long TOTAL_TIME_POINTS = 1_000_000_000;
+	void endTurn() {
+		lock.writeLock().lock();
+		timeSpentBeforeThisTurn += getTimeSpentThisTurn();
+		isRunning = false;
+		lock.writeLock().unlock();
+	}
+
+	boolean isOutOfTime() {
+		lock.readLock().lock();
+		boolean result = timeSpentBeforeThisTurn > TIME_LIMIT;
+		lock.readLock().unlock();
+		return result;
+	}
+
+	private static final long TIME_LIMIT = TimeUnit.SECONDS.toNanos(60);
 
 }
--- src/main/java/org/bitbrawl/foodfight/player/PlayerHook.java
+++ src/main/java/org/bitbrawl/foodfight/player/PlayerHook.java
@@ -2,8 +2,6 @@ package org.bitbrawl.foodfight.player;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.Function;
 import java.util.logging.Logger;
 
@@ -11,36 +9,33 @@ import org.bitbrawl.foodfight.field.Field;
 import org.bitbrawl.foodfight.field.FieldElementState;
 import org.bitbrawl.foodfight.team.Team;
 
-public final class PlayerHook implements AutoCloseable {
+public final class PlayerHook {
 
 	private final JavaPlayer player;
-	private final ClassLoader loader;
 	private final Clock clock;
 
-	private PlayerHook(JavaPlayer player, ClassLoader loader, Clock clock) {
-		this.loader = loader;
+	private PlayerHook(JavaPlayer player, Clock clock) {
 		this.clock = clock;
-		if (loader != null)
-			clocks.put(loader, clock);
 		this.player = player;
 	}
 
 	private PlayerHook(JavaPlayer player) {
 		this.player = player;
-		this.loader = null;
-		this.clock = null;
+		this.clock = new Clock();
 	}
 
-	public static synchronized PlayerHook newPlayerHook(ClassLoader loader, String className, String versionName,
-			Field field, Team team, Inventory inventory, char symbol, float hue, Logger matchLogger,
+	public static PlayerHook newPlayerHook(ClassLoader loader, String className, String versionName, Field field,
+			Team team, Inventory inventory, char symbol, float hue, Logger matchLogger,
 			Function<JavaPlayer, Logger> loggerFunction) throws ClassNotFoundException, NoSuchMethodException,
 			InstantiationException, IllegalAccessException, InvocationTargetException {
 		Clock clock = new Clock();
 		JavaPlayer player;
-		JavaPlayer.setStaticFields(versionName, field, team, clock, inventory, symbol, hue, loggerFunction);
-		player = newPlayer(loader, className, versionName, field, team, clock, inventory, symbol, hue, matchLogger,
-				loggerFunction);
-		return new PlayerHook(player, loader, clock);
+		synchronized (JavaPlayer.class) {
+			JavaPlayer.setStaticFields(versionName, field, team, clock, inventory, symbol, hue, loggerFunction);
+			player = newPlayer(loader, className, versionName, field, team, clock, inventory, symbol, hue, matchLogger,
+					loggerFunction);
+		}
+		return new PlayerHook(player, clock);
 	}
 
 	public static PlayerHook newDummyHook(String versionName, Field field, Team team, char symbol, float hue) {
@@ -64,6 +59,14 @@ public final class PlayerHook implements AutoCloseable {
 		clock.startTurn(turnNumber);
 	}
 
+	public void endTurn() {
+		clock.endTurn();
+	}
+
+	public boolean isOutOfTime() {
+		return clock.isOutOfTime();
+	}
+
 	public void setState(FieldElementState state) {
 		player.setState(state);
 	}
@@ -78,16 +81,6 @@ public final class PlayerHook implements AutoCloseable {
 
 	public void decrementHealth() {
 		player.decrementHealth();
-	}
-
-	@Override
-	public void close() {
-		clocks.remove(loader);
-	}
-
-	public static Clock getClock(Class<?> clazz) {
-		Clock clock = clocks.get(clazz.getClassLoader());
-		return clock == null ? dummyClock : clock;
 	}
 
 	private static JavaPlayer newPlayer(ClassLoader loader, String className, String name, Field field, Team team,
@@ -113,8 +106,5 @@ public final class PlayerHook implements AutoCloseable {
 		};
 
 	}
-
-	private static final Map<ClassLoader, Clock> clocks = new HashMap<>();
-	private static final Clock dummyClock = new Clock();
 
 }
